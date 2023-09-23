94e9dbbde14962a3af6139af64892d93342b83ed
Murad088 <mybrandkross.ru@yandex.ru>
2023-09-23 03:22:55 +0300

    fix subreview 3

--- src/components/burger-ingredients/ingredient.jsx
+++ src/components/burger-ingredients/ingredient.jsx
@@ -90,6 +90,24 @@ export const Ingredient = ({ element, order, setOrder }) => {
   );
 };
 
-Ingredient.propTypes = PropTypes.arrayOf(
-  ingredientPropType.isRequired
-).isRequired;
+Ingredient.propTypes = {
+  element: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    proteins: PropTypes.number,
+    fat: PropTypes.number,
+    carbohydrates: PropTypes.number,
+    calories: PropTypes.number,
+    price: PropTypes.number,
+    image: PropTypes.string.isRequired,
+    image_mobile: PropTypes.string,
+    image_large: PropTypes.string,
+    __v: PropTypes.number,
+  }),
+  order: PropTypes.shape({
+    bun: PropTypes.object,
+    ingredients: PropTypes.array.isRequired,
+  }),
+  setOrder: PropTypes.func.isRequired
+};
